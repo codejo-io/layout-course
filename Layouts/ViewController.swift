671b6ddc6f5453ea7823b24fc5597809c9a09ac8
Matt Casanova <matt@codejo.io>
2020-07-10 08:36:48 -0700

    Saved After ep 2

---
+++ Layouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Layouts
+//
+//  Created by Matt Casanova on 7/10/20.
+//  Copyright © 2020 Codejo.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
